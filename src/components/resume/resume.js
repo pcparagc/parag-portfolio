6f158eac9dd094cfb21cfbd21cab84dc4b3fe5a8
pcparagc <pcparagc@gmail.com>
2023-09-22 00:34:25 -0700

    resume link

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Container, Grid } from '@mui/material';
-import { Document, Page, pdfjs } from 'react-pdf';
-import pdf from '../../assets/pdf/parag-resume.pdf';
-import DownloadIcon from '@mui/icons-material/Download';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import React, { useState, useEffect } from "react";
+import { Button, Container, Grid } from "@mui/material";
+import { Document, Page, pdfjs } from "react-pdf";
+import pdf from "../../assets/pdf/parag-resume.pdf";
+import DownloadIcon from "@mui/icons-material/Download";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
+  "pdfjs-dist/build/pdf.worker.min.js",
+  import.meta.url
 ).toString();
 
 const resumeLink =
-  'https://raw.githubusercontent.com/binh-ngo/react-portfolio/main/src/assets/coding-resume.pdf';
+  "https://github.com/pcparagc/parag-portfolio/blob/main/src/assets/pdf/parag-resume.pdf";
 const Resume = () => {
   const [width, setWidth] = useState(1200);
 
@@ -20,13 +20,13 @@ const Resume = () => {
   }, []);
   return (
     <div>
-      <Container maxWidth="lg" style={{ textAlign: 'center', padding: '20px' }}>
+      <Container maxWidth="lg" style={{ textAlign: "center", padding: "20px" }}>
         <Button
           variant="contained"
           color="primary"
           href={pdf}
           target="_blank"
-          style={{ maxWidth: '250px', margin: '20px' }}
+          style={{ maxWidth: "250px", margin: "20px" }}
           startIcon={<DownloadIcon />}
         >
           Download Resume
